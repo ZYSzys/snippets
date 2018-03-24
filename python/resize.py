d21b4541306b1a3c07f9f4e1c55d0f979a7bc538
ZYSzys <17367077526@163.com>
2018-03-24 20:17:02 +0800

    :pencil2:update resize.py

--- python/resize.py
+++ python/resize.py
@@ -3,4 +3,10 @@ from PIL import Image
 
 def reduce_size(img):
 	im = img.resize((160, 60), Image.ANTIALIAS)
+	# im.save('xxx.jpg')
 	return np.array(im)
+
+if __name__ == '__main__':
+	img = Image.open('xxx.jpg')
+	res = reduce_size(img)
+	print(res)
