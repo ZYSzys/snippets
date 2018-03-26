d5099bdab37172cce4099ff7f2b8043a34a0e325
ZYSzys <17367077526@163.com>
2018-03-26 19:00:09 +0800

    adjust

--- python/resize.py
+++ python/resize.py
@@ -2,11 +2,12 @@ import numpy as np
 from PIL import Image
 
 def reduce_size(img):
-	im = img.resize((160, 60), Image.ANTIALIAS)
-	# im.save('xxx.jpg')
+	im = img.resize((320, 240), Image.ANTIALIAS)
+	im.show()
+	# im.save('q&a.jpg')
 	return np.array(im)
 
 if __name__ == '__main__':
-	img = Image.open('xxx.jpg')
+	img = Image.open('q&a.jpeg')
 	res = reduce_size(img)
 	print(res)
