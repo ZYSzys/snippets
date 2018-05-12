7abe8c0cd5a7eb81b7ae459b23fa45074f8dbd79
ZYSzys <17367077526@163.com>
2018-05-12 21:00:15 +0800

    :smiley: add moduleTemplate

---
+++ javascript/README.md
@@ -0,0 +1,10 @@
+# javascript snippets
+
+数组差异元素
+- [diffArray.js](diffArray.js)
+
+模块模板 
+- [moduleTemplate.js](moduleTemplate.js)
+
+简单服务器脚本
+- [server.js](server.js)
---
+++ javascript/moduleTemplate.js
@@ -0,0 +1,84 @@
+(function() {
+  var root = (typeof self === 'object' && self.self === self && self) ||
+      (typeof global === 'object' && global.global === global && global) ||
+      this || {}
+
+  var util = {
+    extend: function(target) {
+      for (var i = 1; i < arguments.length; i++) {
+        for (var prop in arguments[i]) {
+          if (arguments[i].hasOwnProperty(prop)) {
+            target[prop] = arguments[i][prop]
+          }
+        }
+      }
+      return target
+    },
+    indexOf: function(array, item) {
+      if (array.indexOf) {
+        return array.indexOf(item)
+      } else {
+        var res = -1
+        for (var i = 0; i < array.lenth; i++) {
+          if (array[i] === item) {
+            res = i
+            break;
+          }
+        }
+        return res
+      }
+    },
+    isValidListener: function(listener) {
+      if (typeof listener === 'function') {
+        return true
+      } else if (listener && typeof listener === 'object') {
+        return util.isValidListener(listener.listener)
+      } else {
+        return false
+      }
+    }
+    /*
+    getViewPortSizeHeight: function() {
+      var w = window
+      if (w.innerWidth != null)
+        return w.innerHeight
+      //标准模式
+      var d = document
+      if (document.compatMode === 'CSS1Compat') {
+        return d.documentElement.clientHeight
+      } else {
+        //怪异模式
+        return d.body.clientHeight
+      }
+    },
+    getScrollOffsetTop: function() {
+      var w = window
+      if (w.pageXOffset != null)
+        return w.pageYOffset
+      var d = w.document
+      if (document.compatMode === 'CSS1Compat') {
+        return d.documentElement.scrollTop
+      } else {
+        return d.body.scrollTop
+      }
+    },
+    */
+  }
+
+  function moduleName(options) {
+    this.options = util.extend({}, this.constructor.defaultOptions, options)
+    this.handlers = {}
+    //this.init()
+  }
+  moduleName.VERSION = '1.0.0'
+  moduleName.defaultOptions = {}
+
+  if (typeof exports != 'undefined' && !exports.nodeType) {
+    if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+      exports = module.exports = moduleName;
+    }
+    exports = moduleName;
+  } else {
+    root.moduleName = moduleName;
+  }
+}())
