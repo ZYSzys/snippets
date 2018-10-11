2cd6d670ec378bb53dfbd53baf1fe3bbb6824a47
ZYSzys <17367077526@163.com>
2018-10-11 20:39:46 +0800

    :sparkles: chore

--- python/resize.py
+++ python/resize.py
@@ -1,13 +1,16 @@
 import numpy as np 
 from PIL import Image
 
-def reduce_size(img):
-	im = img.resize((320, 240), Image.ANTIALIAS)
-	im.show()
-	# im.save('q&a.jpg')
+def reduce_size(img, idx, h):
+	im = img.resize((640, int(h)), Image.ANTIALIAS)
+	# im.show()
+	im.save('zzz'+ idx + '.jpg')
 	return np.array(im)
 
 if __name__ == '__main__':
-	img = Image.open('q&a.jpeg')
-	res = reduce_size(img)
-	print(res)
+  idx = 1
+  for i in range(1, 15):
+    img = Image.open(str(i) + '.jpg')
+    h = img.size[1] / (img.size[0]/640)
+    res = reduce_size(img, str(i), h)
+    print(res)
